456faf13d95101776796f59c79925b351f3bf048
Anshul <kushwahasg4450@gmail.com>
2022-12-08 18:10:55 +0530

    checkprime

---
+++ PrimeProblem.js
@@ -0,0 +1,17 @@
+
+let num 19;
+
+count = 0;
+
+for (let i=2; i<=num; i+){
+    if(num%i==0){
+        count+
+    }
+}
+if(count==1){
+    console.log("Yes")
+    
+}
+else{
+    console.log("No")
+}
